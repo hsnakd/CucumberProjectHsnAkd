862745b7655c0eaba45c9b6254feff9179149d73
hsnakd <hsnakd10@gmail.com>
2022-10-04 03:58:49 +0300

    day 17

--- src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
@@ -1,22 +1,44 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.BasePage;
+import com.cydeo.pages.OrderPage;
+import com.cydeo.pages.WebTableLoginPage;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.Select;
 
 public class Order_StepDefinitions {
+
+    WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
+    BasePage basePage = new BasePage();
+    OrderPage orderPage = new OrderPage();
+
     @Given("user is already logged in and on order page")
     public void user_is_already_logged_in_and_on_order_page() {
+        //Getting the page using the URL from configuration.properties
+        Driver.getDriver().get(ConfigurationReader.getProperty("web.table.url"));
 
+        //Calling our login method to log in to web table app
+        webTableLoginPage.login();
+
+        //clicking to "order" link to go order page
+        basePage.order.click();
     }
+
     @When("user selects product type {string}")
     public void user_selects_product_type(String string) {
+        Select select = new Select(orderPage.productDropdown);
+        select.selectByVisibleText(string);
+    }
+
+    @When("user enters quantity {string}")
+    public void user_enters_quantity(String string) {
 
     }
-    @When("user enters quantity {int}")
-    public void user_enters_quantity(Integer int1) {
 
-    }
     @When("user enters customer name {string}")
     public void user_enters_customer_name(String string) {
 
